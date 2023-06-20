d8c1c857c0ed57474f73fc2e5c5da876447c7632
Benedikt Volkmer <benedikt.volkmer@nuromedia.com>
2023-06-20 12:18:34 +0200

    Add fork and screen options

--- run_configurations.py
+++ run_configurations.py
@@ -2,9 +2,12 @@
 
 import logging
 import os
+import subprocess
 import stat
 import sys
 from pathlib import Path
+from typing import Any
+from shutil import which
 
 import click
 
@@ -146,6 +149,24 @@ def set_log_level(ctx, _, value) -> None:
 @click.argument("run_config", type=RunConfigType())
 @click.argument("args", nargs=-1)
 @click.option(
+    "--fork",
+    "-f",
+    is_flag=True,
+    help="Fork process and return immediately. If -s is also supplied the screen session will start detached.",
+)
+@click.option(
+    "--null-pipe",
+    "-n",
+    is_flag=True,
+    help="Use a null pipe instead of a PTY. Is ignored if -s is supplied",
+)
+@click.option(
+    "--screen",
+    "-s",
+    is_flag=True,
+    help="Run in a screen session.",
+)
+@click.option(
     "--edit",
     "-e",
     is_flag=True,
@@ -219,11 +240,19 @@ def cli(
     base_dir: Path,
     make_executable: bool = False,
     edit: bool = False,
+    fork: bool = False,
+    null_pipe: bool = False,
+    screen: bool = False,
 ) -> None:
     """Run a run config
 
     A run config can be any executable file in the .run_configs directory.
     """
+    if null_pipe and screen:
+        logging.warning("Ignoring null pipe because screen is enabled.")
+    if screen and which("screen") is None:
+        logging.error("screen not found. Please install screen.")
+        ctx.exit(1)
     logging.debug(f"Parameter: {ctx.params}")
     rc_dir = get_rc_dir(base_dir)
     logging.debug(f"Base dir: {base_dir}")
@@ -263,11 +292,27 @@ def cli(
                 str(rc.absolute()), os.stat(str(rc.absolute())).st_mode | stat.S_IEXEC
             )
     logging.debug(f"Changing directory to {base_dir}")
-    os.chdir(base_dir)
-    args_list = list(args) if len(args) > 0 else []
-    logging.debug(f"Executing {rc} with args {args_list}")
+    prog_args = list(args) if len(args) > 0 else []
+    logging.debug(f"Executing {rc} with args {prog_args}")
+    prog = str(rc.absolute())
+    prog_args.insert(0, prog)
+    if screen:
+        prog_args.insert(0, "screen")
+        if fork:
+            prog_args.insert(1, "-d")
+            prog_args.insert(2, "-m")
+    logging.debug(f"Executing {prog_args}")
+
+    process_args: dict[str, Any] = dict(cwd=base_dir, env=os.environ)
+    if null_pipe and not screen:
+        process_args["stdout"] = subprocess.DEVNULL
+        process_args["stderr"] = subprocess.DEVNULL
     try:
-        os.execv(str(rc.absolute()), [str(rc)] + args_list)
+        proc = subprocess.Popen(
+            prog_args, start_new_session=fork and not screen, **process_args
+        )
+        if not fork:
+            proc.wait()
     except OSError as e:
         logging.error(f"Error executing {rc}: {e}")
         raise click.FileError(rc, f"{e}")
